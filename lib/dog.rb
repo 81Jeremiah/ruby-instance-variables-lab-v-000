9ece6dd21d5c2096b0aa73910f2e4b1e242b78bd
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-26 00:15:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-  attr_accessor :name
+  attr_accessor :this_dogs_name
   # def name=(dog_name)
   #   @this_dogs_name = dog_name
   # end
-  # 
+  #
   # def name
   #   @this_dogs_name
   # end
